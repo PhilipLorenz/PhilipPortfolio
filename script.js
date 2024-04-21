756e254b4cd5722ba8a40778285b8d2f3664f307
PhilipLorenz <philip.sarmiento72@gmail.com>
2024-04-21 09:51:37 +0800

    efficient way

--- script.js
+++ script.js
@@ -4,16 +4,3 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-
-function downCV(){
-    /*const pdfUrl = 'assets/PLDS_CV.pdf';
-    var a = document.createElement('a');
-    a.href = pdfUrl;
-    a.download = 'PLDS_CV.pdf';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);*/
-}
-function goToLinkdIn(){
-    window.location="https://www.linkedin.com/in/philiplorenzdlsarmiento/"
-}
